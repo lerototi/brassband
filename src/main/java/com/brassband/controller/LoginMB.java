2aa083a2d5fc54bfbd733ae4d136f7d965b96c47
Leonardo Torres <lerototi@gmail.com>
2018-09-10 00:56:13 -0300

    Add current user identification in the menu

--- src/main/java/com/brassband/controller/LoginMB.java
+++ src/main/java/com/brassband/controller/LoginMB.java
@@ -2,33 +2,44 @@ package com.brassband.controller;
 
 import javax.faces.bean.ManagedBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import com.brassband.model.User;
+import com.brassband.service.UserService;
 
 @ManagedBean
+@Component
 public class LoginMB {
 
-	public LoginMB() {
-		getUserLoginInfo();
-	}
-
-	private User user;
+	@Autowired
+	private UserService userService;
+	private User currentUser;
 
 	// Object principal =
 	// SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-	public void getUserLoginInfo() {
-		SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		//System.out.println(userDetails.getUsername());
+	public String getCurrentUserName() {
+		
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		return userDetails.getUsername();
 	}
 
-	public User getUser() {
-		return user;
+	public User getCurrentUser() {
+		
+		if(currentUser== null) {
+			 currentUser = userService.findByMail(getCurrentUserName());
+			 System.out.println("service "+currentUser.getPflUser().getName());
+			return currentUser;
+			
+		}else
+			
+		return currentUser;
 	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
+	
 }
